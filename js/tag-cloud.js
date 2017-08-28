13b6b470144e5fb021ccbddbda2e3cc93f6cec7d
lichuhui <lichuhui@gmail.com>
2017-08-29 02:35:35 +0800

    更新分类页面。

--- js/tag-cloud.js
+++ js/tag-cloud.js
@@ -1,37 +1,30 @@
 $(document).ready(function () {
-  const $content = $('#content');
   const $container = $('#tag-cloud-container');
-
-  if ($.contains($content[0], $container[0])) {
-    $('#tags').slideUp('fast',function(){
-      $(this).remove();
-    });
-  }
-
   const $tagCloud = $('#tag-cloud');
+  const tagPathMap = TAG_CLOUD_DATA.tagPathMap;
   const TAG_CLOUD = {
     _$tags: $container,
     _$tagCloud: $tagCloud,
     _reSize: function () {
-      var mw = this._$tags.innerWidth();
-      var w = TAG_CLOUD_LIST.length * 10;
+      var mw = this._$tags.innerWidth() - 14;
+      var w = TAG_CLOUD_DATA.tagArray.length * 10;
       w = w < 250 ? 250 : w;
       w = w > mw ? mw : w;
       this._$tagCloud.css({'width': w, 'height': w});
     },
     _generateTagCloud: function () {
       WordCloud($tagCloud[0], {
-        list: TAG_CLOUD_LIST,
-        gridSize: 16,
-        minSize: '12px',
-        weightFactor: 16,
+        list: TAG_CLOUD_DATA.tagArray,
+        gridSize: 8,
+        minSize: '1rem',
+        weightFactor: 12,
         classes: 'tag-cloud-span',
         color: 'random-dark',
         /*color: function() {
           return (['#000000','#7FDBFF','#0074D9','#F012BE','#2ECC40','#01FF70','#85144B','#001F3F','#3D9970','#FF851B','#B10DC9','#FF4136','#DDDDDD','#AAAAAA','#39CCCC','#FFDC00'])[Math.floor(Math.random() * 16)]
         },*/
         shape: 'circle',
-        backgroundColor: '#f8f9fa',
+        backgroundColor: '#FFF',
         rotateRatio: 0.5,
         //rotationSteps: 2,
         hover: function (item) {
@@ -45,10 +38,10 @@ $(document).ready(function () {
         },
         click: function(item) {
           if (item) {
-            window.location.href = decodeURIComponent(window.location.href) + item[0] + '/';
+            window.location.href = '/' + tagPathMap[item[0]];
           }
         }});
-    }
+    },
   };
   TAG_CLOUD._reSize();
   TAG_CLOUD._generateTagCloud();
---
+++ vendor/prism/plugins/line-numbers/prism-line-numbers.js
@@ -0,0 +1,70 @@
+(function() {
+
+if (typeof self === 'undefined' || !self.Prism || !self.document) {
+	return;
+}
+
+Prism.hooks.add('complete', function (env) {
+
+	if (!env.code) {
+		return;
+	}
+
+	// works only for <code> wrapped inside <pre> (not inline)
+	var pre = env.element.parentNode;
+
+	/* fixed by lichuhui https//lichuhui.github.io
+	var clsReg = /\s*\bline-numbers\b\s*!/;
+	if (
+		!pre || !/pre/i.test(pre.nodeName) ||
+			// Abort only if nor the <pre> nor the <code> have the class
+		(!clsReg.test(pre.className) && !clsReg.test(env.element.className))
+	) {
+		return;
+	}
+	*/
+
+	if (env.element.querySelector(".line-numbers-rows")) {
+		// Abort if line numbers already exists
+		return;
+	}
+
+	/* fixed by lichuhui https//lichuhui.github.io
+	if (clsReg.test(env.element.className)) {
+		// Remove the class "line-numbers" from the <code>
+		env.element.className = env.element.className.replace(clsReg, '');
+	}
+	if (!clsReg.test(pre.className)) {
+		// Add the class "line-numbers" to the <pre>
+		pre.className += ' line-numbers';
+	}
+	*/
+  if (env.element.classList.contains('line-numbers')) {
+    // Remove the class "line-numbers" from the <code>
+    env.element.classList.remove('line-numbers');
+  }
+  if (!pre.classList.contains('line-numbers')) {
+    // Add the class "line-numbers" to the <pre>
+    pre.classList.add('line-numbers');
+  }
+
+	var match = env.code.match(/\n(?!$)/g);
+	var linesNum = match ? match.length + 1 : 1;
+	var lineNumbersWrapper;
+
+	var lines = new Array(linesNum + 1);
+	lines = lines.join('<span></span>');
+
+	lineNumbersWrapper = document.createElement('span');
+	lineNumbersWrapper.setAttribute('aria-hidden', 'true');
+	lineNumbersWrapper.className = 'line-numbers-rows';
+	lineNumbersWrapper.innerHTML = lines;
+
+	if (pre.hasAttribute('data-start')) {
+		pre.style.counterReset = 'linenumber ' + (parseInt(pre.getAttribute('data-start'), 10) - 1);
+	}
+
+	env.element.appendChild(lineNumbersWrapper);
+});
+
+}());
